15e53a1c5725b1c32287f4291fb1ec10bfb11761
Eduardo Resende <ed.resende@gmail.com>
2017-12-05 20:47:29 -0200

    Data binding

---
+++ 04-data-binding/app.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    website: 'http://www.google.com',
+    title: 'The google link',
+    name: 'Edu',
+    amazonlink: '<a href="http://www.amazon.com">amazon</a>'
+  },
+});
